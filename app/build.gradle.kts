c5ec0cef7ca25c9a72bb38985a4b81cf8a52857d
yishaic11 <yishai.chen28@gmail.com>
2026-03-12 17:30:26 +0200

    Added navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.pluribook"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.pluribook"
@@ -43,6 +41,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
