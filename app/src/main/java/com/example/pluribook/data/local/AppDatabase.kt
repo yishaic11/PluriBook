6512d1dea39195e253f5da357d01f16f6478f5ec
yishaic11 <yishai.chen28@gmail.com>
2026-03-14 21:11:28 +0200

    Updated version

--- app/src/main/java/com/example/pluribook/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/pluribook/data/local/AppDatabase.kt
@@ -8,7 +8,7 @@ import com.example.pluribook.data.model.Post
 import com.example.pluribook.data.model.Comment
 
 @TypeConverters(Converters::class)
-@Database(entities = [User::class, Post::class, Comment::class], version = 3, exportSchema = false)
+@Database(entities = [User::class, Post::class, Comment::class], version = 4, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun postDao(): PostDao
