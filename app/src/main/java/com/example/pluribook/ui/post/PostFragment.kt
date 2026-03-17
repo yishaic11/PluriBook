3097cc359b7dd665bb33956c1c2beaba7100597b
yishaic11 <yishai.chen28@gmail.com>
2026-03-17 20:17:24 +0200

    Fixed highlighted profile nav menu button when clicking other user profile page from home feed

--- app/src/main/java/com/example/pluribook/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/pluribook/ui/post/PostFragment.kt
@@ -138,12 +138,16 @@ class PostFragment : Fragment(R.layout.fragment_post) {
                     val count = post.likedBy.size
                     textLikesCount.text =
                         resources.getQuantityString(R.plurals.likes_plural, count, count)
-
                     val navigateToProfile = View.OnClickListener {
                         val bundle = Bundle().apply {
                             putString("targetUserId", post.senderId)
                         }
-                        findNavController().navigate(R.id.profile_fragment, bundle)
+
+                        if (post.senderId == viewModel.currentUserId) {
+                            findNavController().navigate(R.id.profile_fragment, bundle)
+                        } else {
+                            findNavController().navigate(R.id.other_profile_fragment, bundle)
+                        }
                     }
 
                     textSenderName.setOnClickListener(navigateToProfile)
