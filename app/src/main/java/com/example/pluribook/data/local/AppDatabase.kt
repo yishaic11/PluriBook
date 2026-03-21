f9818cbaad79587cbdb48f3dd659042e65fa17d9
YaelAbbo <yaelabbo@gmail.com>
2026-03-21 16:52:29 +0200

    fix version of database

--- app/src/main/java/com/example/pluribook/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/pluribook/data/local/AppDatabase.kt
@@ -8,7 +8,7 @@ import com.example.pluribook.data.model.Post
 import com.example.pluribook.data.model.Comment
 
 @TypeConverters(Converters::class)
-@Database(entities = [User::class, Post::class, Comment::class], version = 6, exportSchema = false)
+@Database(entities = [User::class, Post::class, Comment::class], version = 7, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun postDao(): PostDao
